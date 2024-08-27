a8c829cb26239cc5b64c57dc538ecc47c6c795f3
Keshav Maheshwari <beastkeshavmaheshwari51@gmail.com>
2024-08-27 14:42:40 +0530

    Update Car.java

--- Applet/Car.java
+++ Applet/Car.java
@@ -6,19 +6,19 @@ import java.awt.*;
 </applet>
 */
 
-public class Car extends Applet
+public class MyDemo extends Applet
 {
     public void paint(Graphics g)
     {
-	g.drawLine(50,50,25,120);
+	g.drawLine(50,50,0,120);
 	g.setColor(Color.yellow);
 	g.fillRect(50,50,50,70);
 	g.setColor(Color.black);
 	g.drawLine(100,50,150,120);
 	g.setColor(Color.blue);
-	g.fillRect(20,120,140,50);
+	g.fillRect(0,120,160,50);
 	g.setColor(Color.black);
-	g.fillOval(30,165,40,40);
+	g.fillOval(10,165,40,40);
 	g.fillOval(110,165,40,40);
     }
 }
